7933d3e46d26e194610d4665e674907e85b15c91
Jean Montero <jean@IE0117.myguest.virtualbox.org>
2024-06-19 23:45:24 -0600

    Se agregan los headers en game.c, pendiente la busqueda de logica relacionada a lo que se desea hacer, los headers incluidos son los que a priori se van a utilizar ya que pueden agregarse mas en el transcurso del desarrollo

---
+++ game.c
@@ -0,0 +1,10 @@
+/*
+ * Primeramente se incluye el headers game.h ya que contiene HEIGHT y WIDTH junto con otros prototipos y definiciones
+ * Seguidamente, stdlib en donde hay bibliotecas comunes
+ * Con el uso de time.h se plantea usar para generar numeros aleatorios
+ */
+#include "game.h"
+#include <stdlib.h>
+#include <time.h>
+
+
