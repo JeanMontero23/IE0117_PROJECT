a5912e89bce534204e5ab366d8cbe24f8eed3f1c
Jean Montero <jean@IE0117.myguest.virtualbox.org>
2024-06-28 21:04:46 -0600

    Se realizan multiples cambios a main.c ya que habian varios errores, al trabajar con el archivo a lo largo del dia la similitud con el main.c del inicio cambio considerablemente

--- main.c
+++ main.c
@@ -1,74 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
-#include "game.h"
+#include "buscaminas.h"
 
-#define MENU_HEIGHT 10
-#define MENU_WIDTH 40
+#define MENU_HEIGHT 5
+#define MENU_WIDTH 30
+
+void main_menu();
 
 int main() {
-    WINDOW *menu_win;
-    int startx, starty;
-    int ch;
-
     initscr();
     clear();
     noecho();
     cbreak();
-    curs_set(0);
 
-    startx = (COLS - MENU_WIDTH) / 2;
-    starty = (LINES - MENU_HEIGHT) / 2;
-
-    menu_win = newwin(MENU_HEIGHT, MENU_WIDTH, starty, startx);
-    keypad(menu_win, TRUE);
-    refresh();
-
-    mvprintw(0, 0, "Bienvenido a Buscaminas!");
-    mvprintw(1, 0, "Usa las flechas para navegar y Enter para seleccionar una opción.");
-    mvprintw(2, 0, "Presiona 'q' para salir.");
-    refresh();
-
-    char *choices[] = {
-        "Iniciar Juego",
-        "Instrucciones",
-        "Salir"
-    };
-    int n_choices = sizeof(choices) / sizeof(char *);
-    int choice = 0;
-
-    while (1) {
-        for (int i = 0; i < n_choices; ++i) {
-            if (i == choice) {
-                wattron(menu_win, A_REVERSE);
-            }
-            mvwprintw(menu_win, i + 1, 1, "%s", choices[i]);
-            wattroff(menu_win, A_REVERSE);
-        }
-        wrefresh(menu_win);
-
-        ch = wgetch(menu_win);
-        switch (ch) {
-            case KEY_UP:
-                if (choice > 0) choice--;
-                break;
-            case KEY_DOWN:
-                if (choice < n_choices - 1) choice++;
-                break;
-            case 10:  // Enter key
-                if (choice == 0) {
-                    start_game(menu_win);
-                } else if (choice == 1) {
-                    show_instructions(menu_win);
-                } else if (choice == 2) {
-                    endwin();
-                    return 0;
-                }
-                break;
-            case 'q':
-                endwin();
-                return 0;
-        }
-    }
+    main_menu();
 
     endwin();
     return 0;
+}
+
+void main_menu() {
+    WINDOW *menu_win;
+    int highlight = 1;
+    int choice = 0;
+    int c;
+    int startx, starty;
+
+    startx = (80 - MENU_WIDTH) / 2;
+    starty = (24 - MENU_HEIGHT) / 2;
+
+    menu_win = newwin(MENU_HEIGHT, MENU_WIDTH, starty, startx);
+    keypad(menu_win, TRUE);
+
+    while (1) {
+        clear();
+        refresh();
+        print_menu(menu_win, highlight);
+        while (1) {
+            c = wgetch(menu_win);
+            switch (c) {
+                case KEY_UP:
+                    if (highlight == 1)
+                        highlight = 3;
+                    else
+                        --highlight;
+                    break;
+                case KEY_DOWN:
+                    if (highlight == 3)
+                        highlight = 1;
+                    else
+                        ++highlight;
+                    break;
+                case '\n':
+                    choice = highlight;
+                    break;
+            }
+            print_menu(menu_win, highlight);
+            if (choice != 0)
+                break;
+        }
+        if (choice == 1) {
+            iniciar_juego();
+        } else if (choice == 2) {
+            mostrar_instrucciones();
+        } else if (choice == 3) {
+            break;
+        }
+        choice = 0;
+        highlight = 1;
+    }
 }
